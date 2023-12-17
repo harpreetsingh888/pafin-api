c4c1174c4b199b70b9c705772116e6832931a1fa
Harpreet Singh <harpreetsingh888@gmail.com>
2023-12-18 01:17:02 +1300

    Add node container to docker

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -3,20 +3,20 @@ import jwt from 'jsonwebtoken';
 import { Request, Response, NextFunction } from 'express';
 
 export function generateToken(id: string): string {
-  return jwt.sign({ id }, 'secret');
+    return jwt.sign({ id }, 'secret');
 }
 
-export function authenticate(req: Request & { user?: any }, res: Response, next: NextFunction): Response<any, Record<string, any>> | undefined  {
-  const token = req.header('x-auth-token');
-  if (!token) {
-    return res.status(401).send('Access denied. No token provided.');
-  }
+export function authenticate(req: Request & { user?: any }, res: Response, next: NextFunction): Response<any, Record<string, any>> | undefined {
+    const token = req.header('x-auth-token');
+    if (!token) {
+        return res.status(401).send('Access denied. No token provided.');
+    }
 
-  try {
-    const payload = jwt.verify(token, 'secret');
-    req.user = payload;
-    next();
-  } catch (ex) {
-    return res.status(400).send('Invalid token.');
-  }
+    try {
+        const payload = jwt.verify(token, 'secret');
+        req.user = payload;
+        next();
+    } catch (ex) {
+        return res.status(400).send('Invalid token.');
+    }
 }
